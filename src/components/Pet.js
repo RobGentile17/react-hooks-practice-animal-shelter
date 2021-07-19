0337f3f9c860e525543f794d0343e85977c214cf
Rob <robert.gentile@powerhrg.com>
2021-07-19 11:13:52 -0400

    Completed Assignment

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,24 +1,30 @@
 import React from "react";
 
-function Pet() {
+function Pet({ pet, onAdoptPet }) {
+  function handleAdoptedClick() {
+    onAdoptPet(pet.id);
+  }
   return (
     <div className="card">
       <div className="content">
         <span className="header">
-          {/*'♀' OR '♂' */}
-          PET NAME
+        {pet.gender === "female" ? "♀" : "♂"}
+          {pet.name}
         </span>
         <div className="meta">
-          <span className="date">PET TYPE</span>
+          <span className="date">{pet.type}</span>
         </div>
         <div className="description">
-          <p>Age: PET AGE</p>
-          <p>Weight: PET WEIGHT</p>
+          <p>Age: {pet.age}years old</p>
+          <p>Weight: {pet.weight}lbs</p>
         </div>
       </div>
       <div className="extra content">
+        {pet.isAdopted ? (
         <button className="ui disabled button">Already adopted</button>
-        <button className="ui primary button">Adopt pet</button>
+        ) : (
+        <button className="ui primary button" onClick={handleAdoptedClick}>Adopt pet</button>
+        )}
       </div>
     </div>
   );
